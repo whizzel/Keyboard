ee2b26ea95f63d6cb1530155dbae785b65608763
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2026-03-22 07:43:43 +0530

    feat: adding simple favicon.ico

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Contact() {
   return (
     <div className="mt-8 ">
-      <Link href={"https://github.com/whizzel/"} className="text-gray-700 hover:text-gray-900">
+      <Link href={"https://github.com/whizzel/Keyboard"} className="text-gray-700 hover:text-gray-900">
         @Vishal
       </Link>
     </div>
